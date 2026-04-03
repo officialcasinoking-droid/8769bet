272d8c49604eab3242213616409f9eb21f42efa5
8769bet-dev <dev@8769bet.local>
2026-04-03 08:26:56 -0700

    Fix SPA routing: switch frontend to HashRouter, remove routes from render.yaml

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
     <Toaster 
       position="top-center"
@@ -25,5 +25,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         },
       }}
     />
-  </BrowserRouter>
+  </HashRouter>
 )
